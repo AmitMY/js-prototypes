c3d6058626dae19a439265a5b377a2f288c6664e
Amit Moryossef <amitmoryossef@gmail.com>
2016-12-31 19:32:07 +0200

    Added string capitalise

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import arrayPrototypes from "src/array.js";
-import objectPrototypes from "src/object.js";
+import arrayPrototypes from "./src/array.js";
+import objectPrototypes from "./src/object.js";
+import stringPrototypes from "./src/string.js";
 
 export default class {
     static all() {
         this.array();
         this.object();
+        this.string();
     }
 
     static array() {
         arrayPrototypes();
     }
-    
+
     static object() {
         objectPrototypes();
+    }
+
+    static string() {
+        stringPrototypes();
     }
 }
