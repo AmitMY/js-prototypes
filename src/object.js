7b25054e28dd728df6dffdd6a853fd9fe70d3c1e
Amit Moryossef <amitmoryossef@gmail.com>
2017-01-03 18:49:14 +0200

    Changed object methods to prototypes

--- src/object.js
+++ src/object.js
@@ -1,19 +1,23 @@
-export default function () {
-    Object.forEach = function (obj, callback) {
+export default function (testCollision) {
+    let methods = ["clone", "forEach", "size", "values"];
+    testCollision(Object, methods);
+
+    Object.prototype.clone = function (obj) {
+        return JSON.parse(JSON.stringify(obj));
+    };
+
+    Object.prototype.forEach = function (obj, callback) {
         for (let key in obj)
             if (obj.hasOwnProperty(key))
                 callback(obj[key], key);
     };
 
-    Object.size = function (obj) {
+    Object.prototype.size = function (obj) {
         return Object.keys(obj).length;
     };
 
-    Object.clone = function (obj) {
-        return JSON.parse(JSON.stringify(obj));
-    };
 
-    Object.values = function (obj) {
+    Object.prototype.values = function (obj) {
         return Object.keys(obj).map(key => obj[key]);
     };
 };
