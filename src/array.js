e262d9b3d2bd0651fa34f3a885a47798a783ef06
Amit Moryossef <amitmoryossef@gmail.com>
2017-01-03 18:48:48 +0200

    Added the equals method.

--- src/array.js
+++ src/array.js
@@ -1,6 +1,9 @@
-export default function () {
+export default function (testCollision) {
+    let methods = ["shuffle", "unique", "top", "equals"];
+    testCollision(Array, methods);
+
     Array.prototype.shuffle = function () {
-        return this.sort(()=> 0.5 - Math.random());
+        return this.sort(() => 0.5 - Math.random());
     };
 
     Array.prototype.unique = function () {
@@ -12,4 +15,22 @@ export default function () {
             return null;
         return this[this.length - 1];
     };
+
+    Array.prototype.equals = function (array) {
+        if (!array || !(array instanceof Array) || this.length != array.length)
+            return false;
+
+        this.forEach((value, i) => {
+            if (value instanceof Array && array[i] instanceof Array) {
+                if (!value.equals(array[i]))
+                    return false;
+            } else if (value !== array[i])
+                return false;
+        });
+
+        return true;
+    };
+
+    // Hide method from for-in loops
+    Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 };
